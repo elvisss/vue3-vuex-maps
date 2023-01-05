3e1515f33b47f80ec307b5c1435bef1edb4fe1b7
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-01-04 20:53:09 -0500

    adding styles to searchResults

--- src/components/search-results/SearchResults.ts
+++ src/components/search-results/SearchResults.ts
@@ -1,14 +1,28 @@
-import { defineComponent } from 'vue'
-import { usePlacesStore } from '@/composables'
+import { defineComponent, ref } from 'vue'
+import type { Feature } from '@/interfaces/places'
+import { usePlacesStore, useMapStore } from '@/composables'
 
 export default defineComponent({
   name: 'SearchResults',
   setup() {
     const { isLoadingPlaces, places } = usePlacesStore()
+    const { map } = useMapStore()
+    const activePlace = ref<string>('')
 
     return {
+      activePlace,
       places,
-      isLoadingPlaces
+      isLoadingPlaces,
+      onPlaceClick: (place: Feature) => {
+        activePlace.value = place.id
+
+        const [lng, lat] = place.center
+
+        map.value?.flyTo({
+          zoom: 14,
+          center: [lng, lat]
+        })
+      }
     }
   }
 })
--- src/components/search-results/SearchResults.vue
+++ src/components/search-results/SearchResults.vue
@@ -11,17 +11,25 @@
       <li
         v-for="place in places"
         :key="place.id"
+        @click="onPlaceClick(place)"
         class="list-group-item list-group-item-action text-start"
+        :class="{ active: place.id === activePlace }"
       >
         <h5 class="fs-6">{{ place.text }}</h5>
         <p>{{ place.place_name }}</p>
         <div class="text-end">
-          <button class="btn btn-outline-primary">Directions</button>
+          <button
+            class="btn btn-sm"
+            :class="
+              place.id === activePlace ? 'btn-outline-light' : 'btn-outline-primary'
+            "
+          >
+            Directions
+          </button>
         </div>
       </li>
     </ul>
   </template>
-
 </template>
 
 <style scoped>
